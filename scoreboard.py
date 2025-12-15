5a37a8e6018720ba0ed243d93b173fa767457a4f
Pranav <pranav.25mim10074@vitbhopal.ac.in>
2025-12-15 21:31:15 +0530

    Refactor scoreboard colors and positions

--- scoreboard.py
+++ scoreboard.py
@@ -2,10 +2,14 @@ from turtle import Turtle
 
 
 class Scoreboard(Turtle):
+    
+    SCORE_COLOR = "cyan"
+    SCORE_FONT = ("Courier", 80, "bold") 
 
     def __init__(self):
         super().__init__()
-        self.color("white")
+
+        self.color(self.SCORE_COLOR)
         self.penup()
         self.hideturtle()
         self.l_score = 0
@@ -14,15 +18,22 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.goto(-100, 200)
-        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
-        self.goto(100, 200)
-        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(-100, 250)
+        self.write(self.l_score, align="center", font=self.SCORE_FONT)
+        self.goto(100, 250)
+        self.write(self.r_score, align="center", font=self.SCORE_FONT)
 
     def l_point(self):
         self.l_score += 1
         self.update_scoreboard()
-
+     
     def r_point(self):
         self.r_score += 1
         self.update_scoreboard()
+
+    def game_over(self, winner_message="GAME OVER"):
+        """Displays the Game Over message in the center of the screen."""
+        self.goto(0, 0)
+        self.color("red")
+   
+        self.write(winner_message, align="center", font=("Courier", 60, "bold"))
